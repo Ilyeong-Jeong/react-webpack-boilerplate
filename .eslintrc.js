feca4526971ff27f2fbb8de9cd1dc916d7dc6664
IlYeong Jeong <developjiy@gmail.com>
2024-04-05 14:21:58 +0900

    eslint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'some-other-config-you-use',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
